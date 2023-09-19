2b2c2163f32ec03373cf154f711d17df79eb6c87
Caio Saggioratto Mesquita <103699485+CaioSaggioratto@users.noreply.github.com>
2023-09-19 18:30:41 -0300

    Delete menu.js

--- menu.js
+++
@@ -1,8 +0,0 @@
-//Expandindo menu
-
-var btnExp = document.querySelector('#btn_exp');
-var menuSide = document.querySelector('.menu_lateral')
-
-btnExp.addEventListener('click', function(){
-    menuSide.classList.toggle('expandir')
-})
